ebbcd6d7c7e2fc5d22dc067946edf93841488440
Krevetka0 <92375515+Krevetka0@users.noreply.github.com>
2021-12-05 18:17:33 +0300

    Update PZ_6_3.py

--- PZ_6/PZ_6_3.py
+++ PZ_6/PZ_6_3.py
@@ -1,21 +1,33 @@
 # Дан список размера N. Переставить в обратном порядке элементы список,
 # расположенные между его минимальным и максимальным элементами, включая
 # минимальный и максимальный элементы.
-import random
+from random import randint
 
 spisok = []
 N = int(input('Введите размер списка: '))
-for i in range(N):
-    spisok.append(random.randint(0, 100))
-print('Мы получили список: ', spisok)
-Min = 0
+for i in range(0, N):
+    spisok.append(randint(0, 100))
+print('Мы получили такой список:', spisok)
+
 Max = 0
+Min = 0
 for i in range(len(spisok)):
-    if spisok[i] > spisok[Max]:
+    if spisok[Max] < spisok[i]:
         Max = i
-    elif spisok[i] < spisok[Min]:
+    elif spisok[Min] > spisok[i]:
         Min = i
-b = spisok[Min:(Max + 1)] if Min < Max else spisok[Max:(Min + 1)]
-print('Диапазон от миниамального до максимального:', b)
+
+b = spisok[Max:(Min + 1)] if Min > Max else spisok[Min:(Max + 1)]
+print('Диапазон от минимального до максимального:', b)
 b.reverse()
-print('Развернем наш диапазон', b)
+print('Развернем наш диапазон:', b)
+
+if Min < Max:
+    for i in range(len(b)):
+        del spisok[Min + i]
+        spisok.insert(Min + i, b[i])
+else:
+    for i in range(len(b)):
+        del spisok[Max + i]
+        spisok.insert(Max + i, b[i])
+print('Итоговый список: ', spisok)
