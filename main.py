5746768e8a965526b44111f78c21ea92114d3299
Krevetka <maryvlasenko10@gmail.com>
2022-05-21 15:29:27 +0300

    commit

--- main.py
+++ main.py
@@ -14,75 +14,81 @@ class Main(tk.Frame):
         self.view_records()
 
     def init_main(self):
-        toolbar = tk.Frame(bg='#a0dea0', bd=4)
+        toolbar = tk.Frame(bg='#FFDAB9', bd=4)
         toolbar.pack(side=tk.TOP, fill=tk.X)
 
-        self.add_img = tk.PhotoImage(file="BD/11.gif")
-        self.btn_open_dialog = tk.Button(toolbar, text='Добавить игрока', command=self.open_dialog, bg='#5da130', bd=0,
-                                    compound=tk.TOP, image=self.add_img, width=90)
+        self.add_img = tk.PhotoImage(file="PZ_16/11.gif")
+        self.btn_open_dialog = tk.Button(toolbar, text='Добавить книгу', command=self.open_dialog, bg='#FFEFD5', bd=0,
+                                         compound=tk.TOP, image=self.add_img, width=90)
         self.btn_open_dialog.pack(side=tk.LEFT)
 
-        self.update_img = tk.PhotoImage(file="BD/12.gif")
-        btn_edit_dialog = tk.Button(toolbar, text="Редактировать", command=self.open_update_dialog, bg='#5da130',
+        self.update_img = tk.PhotoImage(file="PZ_16/12.gif")
+        btn_edit_dialog = tk.Button(toolbar, text="Редактировать", command=self.open_update_dialog, bg='#FFEFD5',
                                     bd=0, compound=tk.TOP, image=self.update_img, width=90)
         btn_edit_dialog.pack(side=tk.LEFT)
 
-        self.search_img = tk.PhotoImage(file="BD/13.gif")
-        btn_search_dialog = tk.Button(toolbar, text="Поиск", command=self.open_search_dialog, bg='#5da130',
-                                    bd=0, compound=tk.TOP, image=self.search_img, width=90)
+        self.search_img = tk.PhotoImage(file="PZ_16/13.gif")
+        btn_search_dialog = tk.Button(toolbar, text="Поиск", command=self.open_search_dialog, bg='#FFEFD5',
+                                      bd=0, compound=tk.TOP, image=self.search_img, width=90)
         btn_search_dialog.pack(side=tk.LEFT)
 
-        self.delete_img = tk.PhotoImage(file="BD/14.gif")
-        btn_delete = tk.Button(toolbar, text="Удалить запись", command=self.delete_records, bg='#5da130',
-                                    bd=0, compound=tk.TOP, image=self.delete_img, width=90)
+        self.delete_img = tk.PhotoImage(file="PZ_16/14.gif")
+        btn_delete = tk.Button(toolbar, text="Удалить запись", command=self.delete_records, bg='#FFEFD5',
+                               bd=0, compound=tk.TOP, image=self.delete_img, width=90)
         btn_delete.pack(side=tk.LEFT)
 
-        self.refresh_img = tk.PhotoImage(file="BD/15.gif")
-        btn_refresh = tk.Button(toolbar, text="Обновить экран", command=self.view_records, bg='#5da130',
-                               bd=0, compound=tk.TOP, image=self.refresh_img, width=90)
+        self.refresh_img = tk.PhotoImage(file="PZ_16/15.gif")
+        btn_refresh = tk.Button(toolbar, text="Обновить экран", command=self.view_records, bg='#FFEFD5',
+                                bd=0, compound=tk.TOP, image=self.refresh_img, width=90)
         btn_refresh.pack(side=tk.LEFT)
+        self.tree = ttk.Treeview(self, columns=('books_id', 'genre', 'country', 'series', 'author', 'name', 'year',
+                                                'annotation'), height=15, show='headings')
 
-        self.tree = ttk.Treeview(self, columns=('user_id', 'name', 'sex', 'old', 'score'), height=15, show='headings')
+        self.tree.column('books_id', width=40, anchor=tk.CENTER)
+        self.tree.column('genre', width=120, anchor=tk.CENTER)
+        self.tree.column('country', width=120, anchor=tk.CENTER)
+        self.tree.column('series', width=70, anchor=tk.CENTER)
+        self.tree.column('author', width=120, anchor=tk.CENTER)
+        self.tree.column('name', width=120, anchor=tk.CENTER)
+        self.tree.column('year', width=70, anchor=tk.CENTER)
+        self.tree.column('annotation', width=240, anchor=tk.CENTER)
 
-        self.tree.column('user_id', width=50, anchor=tk.CENTER)
-        self.tree.column('name', width=180, anchor=tk.CENTER)
-        self.tree.column('sex', width=140, anchor=tk.CENTER)
-        self.tree.column('old', width=140, anchor=tk.CENTER)
-        self.tree.column('score', width=140, anchor=tk.CENTER)
-
-        self.tree.heading('user_id', text='ID')
-        self.tree.heading('name', text='Имя игрока')
-        self.tree.heading('sex', text='Пол игрока')
-        self.tree.heading('old', text='Возраст игрока')
-        self.tree.heading('score', text='Результат игрока')
+        self.tree.heading('books_id', text='ID')
+        self.tree.heading('genre', text='Жанр')
+        self.tree.heading('country', text='Страна')
+        self.tree.heading('series', text='Серия')
+        self.tree.heading('author', text='Автор')
+        self.tree.heading('name', text='Название')
+        self.tree.heading('year', text='Год')
+        self.tree.heading('annotation', text='Аннотация')
 
         self.tree.pack()
 
-    def records(self, user_id, name, sex, old, score):
-        self.db.insert_data(user_id, name, sex, old, score)
+    def records(self, books_id, genre, country, series, author, name, year, annotation):
+        self.db.insert_data(books_id, genre, country, series, author, name, year, annotation)
         self.view_records()
 
-    def update_record(self, user_id, name, sex, old, score):
-        self.db.cur.execute("""UPDATE users SET user_id=?, name=?, sex=?, old=?, score=? WHERE user_id=?""",
-                            (user_id, name, sex, old, score, self.tree.set(self.tree.selection()[0], '#1')))
+    def update_record(self, books_id, genre, country, series, author, name, year, annotation):
+        self.db.cur.execute("""UPDATE books SET books_id=?, genre=?, country=?, series=?, author=?, name=?,
+         year=?, annotation=? WHERE books_id=?""", (books_id, genre, country, series, author, name, year, annotation,
+                                                    self.tree.set(self.tree.selection()[0], '#1')))
         self.db.con.commit()
-
         self.view_records()
 
     def view_records(self):
-        self.db.cur.execute("""SELECT * FROM users""")
+        self.db.cur.execute("""SELECT * FROM books""")
         [self.tree.delete(i) for i in self.tree.get_children()]
         [self.tree.insert('', 'end', values=row) for row in self.db.cur.fetchall()]
 
-    def search_records(self, score):
-        score = (score,)
-        self.db.cur.execute("""SELECT * FROM users WHERE score>?""", score)
+    def search_records(self, genre):
+        genre = (genre,)
+        self.db.cur.execute("""SELECT * FROM books WHERE genre=?""", genre)
         [self.tree.delete(i) for i in self.tree.get_children()]
         [self.tree.insert('', 'end', values=row) for row in self.db.cur.fetchall()]
 
     def delete_records(self):
         for selection_item in self.tree.selection():
-            self.db.cur.execute("""DELETE FROM users WHERE user_id=?""", (self.tree.set(selection_item, '#1'),))
+            self.db.cur.execute("""DELETE FROM books WHERE books_id=?""", (self.tree.set(selection_item, '#1'),))
         self.db.con.commit()
         self.view_records()
 
@@ -98,7 +104,7 @@ class Main(tk.Frame):
 
 class Child(tk.Toplevel):
 
-    """Класс для дочернего окна"""
+    """"Класс для дочернего окна"""
 
     def __init__(self, root, app):
         super().__init__(root)
@@ -106,46 +112,63 @@ class Child(tk.Toplevel):
         self.view = app
 
     def init_child(self):
-        self.title('Добавить игрока')
-        self.geometry('400x220+400+300')
+        self.title('Добавить книгу')
+        self.geometry('400x300+500+250')
         self.resizable(False, False)
 
-        label_description = tk.Label(self, text='Номер')
-        label_description.place(x=50, y=25)
-        self.entry_description = ttk.Entry(self)
-        self.entry_description.place(x=110, y=25)
+        label_number = tk.Label(self, text='Номер')
+        label_number.place(x=50, y=25)
+        self.entry_number = ttk.Entry(self)
+        self.entry_number.place(x=150, y=25)
 
-        label_name = tk.Label(self, text='Имя')
-        label_name.place(x=50, y=50)
+        label_genre = tk.Label(self, text='Жанр')
+        label_genre.place(x=50, y=50)
+        self.entry_genre = ttk.Entry(self)
+        self.entry_genre.place(x=150, y=50)
+
+        label_country = tk.Label(self, text='Страна')
+        label_country.place(x=50, y=75)
+        self.entry_country = ttk.Entry(self)
+        self.entry_country.place(x=150, y=75)
+
+        label_series = tk.Label(self, text='Серия')
+        label_series.place(x=50, y=100)
+        self.entry_series = ttk.Entry(self)
+        self.entry_series.place(x=150, y=100)
+
+        label_author = tk.Label(self, text='Автор')
+        label_author.place(x=50, y=125)
+        self.entry_author = ttk.Entry(self)
+        self.entry_author.place(x=150, y=125)
+
+        label_name = tk.Label(self, text='Название')
+        label_name.place(x=50, y=150)
         self.entry_name = ttk.Entry(self)
-        self.entry_name.place(x=110, y=50)
+        self.entry_name.place(x=150, y=150)
 
-        label_sex = tk.Label(self, text='Пол')
-        label_sex.place(x=50, y=75)
-        self.combobox = ttk.Combobox(self, values=[u'Мужской', u'Женский'])
-        self.combobox.current(0)
-        self.combobox.place(x=110, y=75)
+        label_year = tk.Label(self, text='Год')
+        label_year.place(x=50, y=175)
+        self.entry_year = ttk.Entry(self)
+        self.entry_year.place(x=150, y=175)
 
-        label_old = tk.Label(self, text='Возраст')
-        label_old.place(x=50, y=100)
-        self.entry_old = ttk.Entry(self)
-        self.entry_old.place(x=110, y=100)
-
-        label_score = tk.Label(self, text='Результат')
-        label_score.place(x=50, y=125)
-        self.entry_score = ttk.Entry(self)
-        self.entry_score.place(x=110, y=125)
+        label_annot = tk.Label(self, text='Аннотация')
+        label_annot.place(x=50, y=200)
+        self.entry_annot = ttk.Entry(self)
+        self.entry_annot.place(x=150, y=200)
 
         btn_cancel = ttk.Button(self, text='Закрыть', command=self.destroy)
-        btn_cancel.place(x=300, y=170)
+        btn_cancel.place(x=300, y=250)
 
         self.btn_ok = ttk.Button(self, text='Добавить')
-        self.btn_ok.place(x=220, y=170)
-        self.btn_ok.bind('<Button-1>', lambda event: self.view.records(self.entry_description.get(),
+        self.btn_ok.place(x=210, y=250)
+        self.btn_ok.bind('<Button-1>', lambda event: self.view.records(self.entry_number.get(),
+                                                                       self.entry_genre.get(),
+                                                                       self.entry_country.get(),
+                                                                       self.entry_series.get(),
+                                                                       self.entry_author.get(),
                                                                        self.entry_name.get(),
-                                                                       self.combobox.get(),
-                                                                       self.entry_old.get(),
-                                                                       self.entry_score.get()))
+                                                                       self.entry_year.get(),
+                                                                       self.entry_annot.get()))
 
         self.grab_set()
         self.focus_set()
@@ -160,12 +183,15 @@ class Update(Child):
     def init_edit(self):
         self.title("Редактировать запись")
         btn_edit = ttk.Button(self, text="Редактировать")
-        btn_edit.place(x=205, y=170)
-        btn_edit.bind('<Button-1>', lambda event: self.view.update_record(self.entry_description.get(),
+        btn_edit.place(x=205, y=250)
+        btn_edit.bind('<Button-1>', lambda event: self.view.update_record(self.entry_number.get(),
+                                                                          self.entry_genre.get(),
+                                                                          self.entry_country.get(),
+                                                                          self.entry_series.get(),
+                                                                          self.entry_author.get(),
                                                                           self.entry_name.get(),
-                                                                          self.combobox.get(),
-                                                                          self.entry_old.get(),
-                                                                          self.entry_score.get()))
+                                                                          self.entry_year.get(),
+                                                                          self.entry_annot.get()))
         self.btn_ok.destroy()
 
 
@@ -176,7 +202,7 @@ class Search(tk.Toplevel):
         self.view = app
 
     def init_edit(self):
-        self.title("Поиск")
+        self.title("Поиск по жанру")
         self.geometry('300x150')
         self.resizable(False, False)
 
@@ -198,19 +224,23 @@ class Search(tk.Toplevel):
 class DB:
     def __init__(self):
 
-        with sq.connect('BD/saper.db') as self.con:
+        with sq.connect('PZ_16/library.db') as self.con:
             self.cur = self.con.cursor()
-            self.cur.execute("""CREATE TABLE IF NOT EXISTS users (
-                user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            self.cur.execute("""CREATE TABLE IF NOT EXISTS books (
+                books_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                genre TEXT NOT NULL,
+                country TEXT NOT NULL,
+                series INTEGER,
+                author TEXT NOT NULL,
                 name TEXT NOT NULL,
-                sex INTEGER NOT NULL DEFAULT 1,
-                old INTEGER,
-                score INTEGER
+                year INTEGER,
+                annotation TEXT NOT NULL
                 )""")
 
-    def insert_data(self, user_id, name, sex, old, score):
-        self.cur.execute("""INSERT INTO users(user_id, name, sex, old, score) VALUES (?, ?, ?, ?, ?)""",
-                             (user_id, name, sex, old, score))
+    def insert_data(self, books_id, genre, country, series, author, name, year, annotation):
+        self.cur.execute("""INSERT INTO books(books_id, genre, country, series, author, name,
+        year, annotation) VALUES (?, ?, ?, ?, ?, ?, ?, ?)""", (books_id, genre, country, series, author, name,
+                                                               year, annotation))
         self.con.commit()
 
 
@@ -219,7 +249,7 @@ if __name__ == "__main__":
     db = DB()
     app = Main(root)
     app.pack()
-    root.title("Работа с базой данных Сапер")
-    root.geometry("650x450+300+200")
+    root.title("Библиотека")
+    root.geometry("900x400+400+200")
     root.resizable(False, False)
     root.mainloop()
